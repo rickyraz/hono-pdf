9d7b6969c127388be3a1e279d80ad15b0e9e9314
rickyraihan <rickyraihan83@gmail.com>
2024-01-19 10:58:39 +0700

    enable-cors

--- server.ts
+++ server.ts
@@ -1,15 +1,12 @@
 import { Hono } from "https://deno.land/x/hono@v3.12.6/mod.ts";
+import { cors } from "https://deno.land/x/hono@v3.12.6/middleware.ts";
 import { PDFDocument } from "https://esm.sh/pdf-lib@1.17.1";
 import { encodeAscii85 } from "https://deno.land/std@0.212.0/encoding/ascii85.ts";
 
 const app = new Hono();
 
-app.use((c, next) => {
-  c.res.headers.set("Access-Control-Allow-Origin", "*"); // Adjust as needed for security
-  c.res.headers.set("Access-Control-Allow-Methods", "GET, POST, OPTIONS");
-  c.res.headers.set("Access-Control-Allow-Headers", "Content-Type");
-  return next();
-});
+// Enable CORS for all routes
+app.use("*", cors());
 
 app.post("/convert-pdf", async (c) => {
   const body = await c.req.arrayBuffer();
@@ -24,7 +21,5 @@ app.options("/convert-pdf", (c) => {
   // An OPTIONS request is made as part of the CORS preflight process.
   return c.text("CORS preflight response");
 });
-
-app.get("/", (c) => c.text("Hello Hono!"));
 
 Deno.serve({ port: 8787 }, app.fetch);
